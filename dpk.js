bd488a3cd3e63fedae5cf3e02c9f146f54b28af1
Kshitij Sharma <skshitij099@gmail.com>
2023-10-31 16:14:53 +0530

    Update dpk.js with correct candidate declaration

--- dpk.js
+++ dpk.js
@@ -4,6 +4,8 @@ exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
 
+  let candidate = event?.partitionKey || event || TRIVIAL_PARTITION_KEY;
+
   if (event) {
     if (event.partitionKey) {
       candidate = event.partitionKey;
@@ -11,8 +13,7 @@ exports.deterministicPartitionKey = (event) => {
       candidate = crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
     }
   }
-  let candidate = event?.partitionKey || event || TRIVIAL_PARTITION_KEY;
-
+  
   if (typeof candidate !== "string") {
     candidate = JSON.stringify(candidate);
   }
